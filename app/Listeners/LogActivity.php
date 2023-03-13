dafe9b9a506d8eb0e0847b36a64e5a9c5f2c3bf2
Wahid Ullah <wahid.ullah.cse@gmail.com>
2023-03-13 17:17:43 +0600

    logout activity

--- app/Listeners/LogActivity.php
+++ app/Listeners/LogActivity.php
@@ -17,13 +17,13 @@ class LogActivity
         $ip = \Request::getClientIp(true);
         //$ua = $request->header('User-Agent');
         $server = \Request::server();
-        $userAgent =  $server['HTTP_USER_AGENT'];
+        $userAgent = $server['HTTP_USER_AGENT'];
         $this->info($event, "User {$event->user->email} logged in from {$ip} {$userAgent}", $event->user->only('id', 'email'));
         try {
             DB::table('authentication_log')->insert([
-                'authenticatable_type' =>'App\Models\User',
-                'authenticatable_id' =>$event->user->id,
-                'ip_address' =>$ip,
+                'authenticatable_type' => 'Login',
+                'authenticatable_id' => $event->user->id,
+                'ip_address' => $ip,
                 'user_agent' => $userAgent,
                 'login_at' => date('Y-m-d H:i:s'),
                 'login_successful' => 1
@@ -35,8 +35,17 @@ class LogActivity
 
     public function logout(LaravelEvents\Logout $event)
     {
+//        $ip = \Request::getClientIp(true);
+//        $this->info($event, "User {$event->user->email} logged out from {$ip}", $event->user->only('id', 'email'));
         $ip = \Request::getClientIp(true);
-        $this->info($event, "User {$event->user->email} logged out from {$ip}", $event->user->only('id', 'email'));
+        try {
+            $result = DB::table('authentication_log')->latest('login_at')->first();
+            DB::table('authentication_log')->where('id', $result->id)->update([
+                'logout_at' => date('Y-m-d H:i:s')
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     public function registered(LaravelEvents\Registered $event)
@@ -47,8 +56,23 @@ class LogActivity
 
     public function failed(LaravelEvents\Failed $event)
     {
+//        $ip = \Request::getClientIp(true);
+//        $this->info($event, "User {$event->credentials['email']} login failed from {$ip}", ['email' => $event->credentials['email']]);
         $ip = \Request::getClientIp(true);
-        $this->info($event, "User {$event->credentials['email']} login failed from {$ip}", ['email' => $event->credentials['email']]);
+        //$ua = $request->header('User-Agent');
+        $server = \Request::server();
+        $userAgent = $server['HTTP_USER_AGENT'];
+        try {
+            DB::table('authentication_log')->insert([
+                'authenticatable_type' => 'Login',
+                'ip_address' => $ip,
+                'user_agent' => $userAgent,
+                'login_at' => date('Y-m-d H:i:s'),
+                'login_successful' => false
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     public function passwordReset(LaravelEvents\PasswordReset $event)
